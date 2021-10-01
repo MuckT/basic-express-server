0c0eba043789a4fa7a1b55c0612341f8fcaf709f
MuckT <mcgtom10@gmail.com>
2021-09-30 19:34:57 -0700

    test: add code cov

--- src/server.test.js
+++ src/server.test.js
@@ -7,19 +7,22 @@ const request = supertest(server.app)
 describe('Given /person', () => {
   describe('When no query string', () => {
     it('Then returns 500 status', async () => {
-      const response = await request.get('/person?test=test')
+      const response = await request.get('/person')
       expect(response.status).toEqual(500)
     })
   })
 
   describe('When name is in query string', () => {
+    let data = {
+      name: 'fred'
+    }
     it('Then returns 200 status', async () => {
-      const response = await request.get('/person?name=fred')
+      const response = await request.get('/person').query(data)
       expect(response.status).toEqual(200)
     })
 
     it('Then returns correct response body', async () => {
-      const response = await request.get('/person?name=fred')
+      const response = await request.get('/person').query(data)
       expect(response.body).toStrictEqual({name: 'fred'})
     })
   })
